7f450512b3a55391ad63917bc90f7207f377c6ab
Gabriel C. <arrobacarvalho@gmail.com>
2024-03-02 15:55:52 -0300

    Incluindo informações de estrutura previa de BD

--- php/conexao.php
+++ php/conexao.php
@@ -4,6 +4,10 @@ $dbname = 'banco';
 $username = 'root';
 $password = 'root';
 
+// Estrutura inicial do BD
+// CREATE DATABASE "banco";
+// CREATE TABLE banco.usuarios (id INT NOT NULL AUTO_INCREMENT, email VARCHAR(255) NOT NULL , nome VARCHAR(255) NOT NULL , senha VARCHAR(255) NOT NULL , PRIMARY KEY (id))
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
